a19ddbfc1b418a08351238dcd0f59fc85074d7e9
minhthanh-206LHP <87489280+minhthanh-206LHP@users.noreply.github.com>
2022-03-29 08:45:37 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var img = new Image();
 img.crossOrigin = 'anonymous';
-img.src = './rhino.jpg';
+img.src = './JujuXTzuYang.jpg';
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 img.onload = function() {
