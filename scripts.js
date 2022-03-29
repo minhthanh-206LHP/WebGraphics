b5ca0598bb183ec3c0b634bae50c9a20ba37025c
minhthanh-206LHP <87489280+minhthanh-206LHP@users.noreply.github.com>
2022-03-29 07:59:03 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,16 +12,16 @@ function invertColor(img)
     var context = canvas.getContext("2d");
 
     context.drawImage(img,0,0);
-    //console.log(img.width,img.height);
-    // context.drawImage(img,img.width,0);
-    var imageData = context.getImageData(0,0,img.width,img.height);
-    var data = imageData.data;
-    for (var i = 0;i< data.length;i+=4)
-    {
-        data[i] = 255-data[i];
-        data[i+1]=255-data[i+1];
-        data[i+2]=255-data[i+2];
-        data[i+3]=255;
-    }
-    context.putImageData(imageData,img.width*2,0);
+
+    context.drawImage(img,img.width,0);
+    //var imageData = context.getImageData(0,0,img.width,img.height);
+    //var data = imageData.data;
+    //for (var i = 0;i< data.length;i+=4)
+    //{
+    //    data[i] = 255-data[i];
+    //    data[i+1]=255-data[i+1];
+    //    data[i+2]=255-data[i+2];
+    //    data[i+3]=255;
+    //}
+    //context.putImageData(imageData,img.width*2,0);
 }
