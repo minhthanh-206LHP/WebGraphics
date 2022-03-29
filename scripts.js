75fea7817838a33fdcc6292ba5f9d6703ea97d3b
minhthanh-206LHP <87489280+minhthanh-206LHP@users.noreply.github.com>
2022-03-29 08:18:38 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,32 @@
-window.onload = function(){
-    var img = new Image();
-    img.onload = function(){
-        invertColor(this);
-    };
-    img.src = "JujuXTzuYang.jpg";
-    img.crossOrigin = 'anonymous';
+var img = new Image();
+img.crossOrigin = 'anonymous';
+img.src = './JujuXTzuYang.jpg';
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+img.onload = function() {
+  ctx.drawImage(img, 0, 0);
+  img.style.display = 'none';
 };
+var hoveredColor = document.getElementById('hovered-color');
+var selectedColor = document.getElementById('selected-color');
 
-function invertColor(img)
-{
-    var canvas = document.getElementById("mycanvas");
-    var context = canvas.getContext("2d");
 
-    context.drawImage(img,0,0);
+function pick(event, destination) {
+  var x = event.layerX;
+  var y = event.layerY;
+  var pixel = ctx.getImageData(x, y, 1, 1);
+  var data = pixel.data;
 
-    //context.drawImage(img,img.width,0);
-    var imageData = context.getImageData(0,0,img.width,img.height);
-    var data = imageData.data;
-    for (var i = 0;i< data.length;i+=4)
-    //{
-    //    data[i] = 255-data[i];
-    //    data[i+1]=255-data[i+1];
-    //    data[i+2]=255-data[i+2];
-    //    data[i+3]=255;
-    //}
-    context.putImageData(imageData,img.width,0);
+	const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+	destination.style.background = rgba;
+	destination.textContent = rgba;
+
+	return rgba;
 }
+
+canvas.addEventListener('mousemove', function(event) {
+	pick(event, hoveredColor);
+});
+canvas.addEventListener('click', function(event) {
+	pick(event, selectedColor);
+});
